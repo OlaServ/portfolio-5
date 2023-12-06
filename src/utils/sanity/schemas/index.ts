de050186ebbf132cc6276753ea01bc6437302f97
Aleksandra Serwotka <aleksandra.serwotka@gmail.com>
2023-12-06 14:40:34 +0100

    set up the project with sanity studio

---
+++ src/utils/sanity/schemas/index.ts
@@ -0,0 +1,8 @@
+import siteConfig from "./siteConfig";
+import navigation from "./documents/navigation";
+import navItem from "./objects/nav-item";
+import link from "./objects/link";
+import page from "./documents/page";
+import blockContent from "./objects/blockContent";
+
+export const schemaTypes = [siteConfig, navigation, navItem, link, page, blockContent]
---
+++ src/utils/sanity/schemas/siteConfig.ts
@@ -0,0 +1,32 @@
+export default {
+  name: 'siteConfig',
+  type: 'document',
+  title: 'Site Settings',
+  __experimental_actions: ['update', 'publish'],
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Site title',
+    },
+    {
+      title: 'URL',
+      name: 'url',
+      type: 'url',
+      description: 'The main site url. Used to create canonical url',
+    },
+    {
+      name: 'frontpage',
+      type: 'reference',
+      description: 'Choose page to be the frontpage',
+      to: {type: 'page'},
+    },
+    {
+      title: 'Main navigation',
+      name: 'mainNav',
+      description: 'Select menu for main navigation',
+      type: 'reference',
+      to: {type: 'navigation'},
+    },
+  ],
+}
