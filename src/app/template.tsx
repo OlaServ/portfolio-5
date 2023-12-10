546c8db88bb35a3f91da33bfab011292067373aa
Aleksandra Serwotka <aleksandra.serwotka@gmail.com>
2023-12-10 18:06:44 +0100

    Removed react query imports

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,20 +2,16 @@
 
 import { FlexProps, Flex } from "@chakra-ui/react";
 import { usePathname } from "next/navigation";
-import { QueryClient, QueryClientProvider } from "react-query";
 import ThemeProvider from "@/app/theme-provider";
 import { Navigation } from "@/modules/navigation/navigation";
 
 export const RouteLayout = ({ children, ...rest }: FlexProps) => {
   const pathname = usePathname();
-  const queryClient = new QueryClient();
-
   if (pathname.includes("admin")) {
     return <>{children}</>;
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <Flex
           overflow="hidden"
@@ -29,7 +25,6 @@ export const RouteLayout = ({ children, ...rest }: FlexProps) => {
           {children}
         </Flex>
       </ThemeProvider>
-    </QueryClientProvider>
   );
 };
 
